6aea93e07ea0924476a31fe09d30516a14b6b95b
github username <github email address>
2020-03-17 12:58:42 -0500

    submission for deadline

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,11 @@ class Movie
 
   def initialize(title)
     @title = title
+    
+    save
+  end
+
+  def save
     self.class.all << self
   end
 
@@ -12,4 +17,23 @@ class Movie
     @@all
   end
 
+  # return an array of reviews for this movie
+  def reviews
+    Review.all.select{|x| x.movie == self }
+  end
+
+  # return an array of viewers that reviewed this movie
+  def reviewers
+    reviews.map{|x| x.viewer }.uniq
+  end
+
+  def average_rating
+    ( reviews.reduce(0){|x, y| x += y.rating }.to_f / reviews.count ) || 0
+  end
+
+  # return the highest rated movie
+  def self.highest_rated
+    # reviews = Review.all
+    Review.reviewed_movies.sort_by{|x| -x.average_rating }.first
+  end
 end
