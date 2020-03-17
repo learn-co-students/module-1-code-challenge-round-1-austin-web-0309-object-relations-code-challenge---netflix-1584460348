643f14e67d77950c553b153058e2c3c1058ca8c4
github username <github email address>
2020-03-17 12:59:07 -0500

    submission for deadline

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,24 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.select { |review| review.movie == self}
+  end
+
+  def reviewers
+    self.reviews.map { |review| review.viewer }
+  end
+
+  def ratings
+    self.reviews.map { |review| review.rating}
+  end
+
+  def average_rating
+    (ratings.sum.to_f) / ratings.size
+  end
+
+  def self.highest_rated
+    Review.all.max_by { |review| review.rating}.movie
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,6 +10,28 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+movie1 = Movie.new('movie1')
+movie2 = Movie.new('movie2')
+movie3 = Movie.new('movie3')
+movie4 = Movie.new('movie4')
+movie5 = Movie.new('movie5')
+movie6 = Movie.new('movie6')
+viewer1 = Viewer.new('viewer1')
+viewer2 = Viewer.new('viewer2')
+viewer3 = Viewer.new('viewer3')
+viewer4 = Viewer.new('viewer4')
+viewer5 = Viewer.new('viewer5')
+viewer6 = Viewer.new('viewer6')
+
+review1 = Review.new(viewer1, movie2, 2)
+review2 = Review.new(viewer2, movie1, 5)
+review3 = Review.new(viewer3, movie3, 4)
+review4 = Review.new(viewer3, movie4, 7)
+review5 = Review.new(viewer4, movie6, 6)
+review6 = Review.new(viewer5, movie2, 3)
+review7 = Review.new(viewer6, movie1, 4)
+
+
 
 
 
