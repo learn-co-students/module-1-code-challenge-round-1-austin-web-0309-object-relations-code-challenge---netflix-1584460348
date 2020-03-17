21c6c8a16a3f343fc37a4f572fd1eef53ec80211
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-03-17 12:57:28 -0500

    submission for deadline

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
+
+
+
 class Movie
   attr_accessor :title
 
@@ -6,6 +9,27 @@ class Movie
   def initialize(title)
     @title = title
     self.class.all << self
+  end
+
+  def reviews
+    Review.all.select{|review| review.movie == self}
+  end
+
+  def reviewers
+    reviews.map{|review| review.viewer}
+  end
+
+  def average_rating
+    reviews.map{|review| review.rating}.inject(:+).to_f / reviews.count
+  end
+
+
+
+
+
+
+  def self.highest_rated
+    self.all.max_by{|movie| movie.average_rating}
   end
 
   def self.all
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,3 +1,6 @@
+
+
+
 class Viewer
   attr_accessor :username
 
@@ -6,6 +9,26 @@ class Viewer
   def initialize(username)
     @username = username
     self.class.all << self
+  end
+
+  def reviews
+    Review.all.select{|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.map{|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+    reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    if (existing_review = reviews.find{|review| review.movie == movie})
+      existing_review.rating = rating
+    else
+      Review.new(self, movie, rating)
+    end
   end
 
   def self.all
