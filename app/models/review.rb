f8f79f98998342c2b4016a7f60e8fa74c61965c4
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-03-17 12:57:21 -0500

    submission for deadline

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,45 @@
 class Review
+    attr_accessor :viewer, :movie, :rating
+    @@all = []
+
+    def initialize (viewer, movie, rating)
+        @viewer = viewer 
+        @movie = movie 
+        @rating = rating 
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+# # returns the Viewer instance associated 
+# # with the Review instance
+
+    def viewer
+        # binding.pry
+        self.select {|person| person == viewer}
+        # self.map {|person| 
+        #     if person  == self.viewer
+        #     self.viewer
+        #     end}
+        #     # binding.pry
+        
+    end
+
+# # returns the Movie instance associated 
+# # with the Review instance
+#     def movie
+#     end
 
 
 end
+
+# self 
+#   <Review:0x00007fc80fb4a198 
+#   @movie=#<Movie:0x00007fc80fb4a350 
+#   @title="Nightmare Before Christmas">, 
+#   @rating=9, 
+#   @viewer=#<Viewer:0x00007fc80fb4a210 
+#   @username="Ramona">>
+
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,24 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+# # returns an array of Review instances 
+# # associated with the Viewer instance.
+# self =  #<Viewer:0x00007ff27024ecc0 @username="Richard">
+  def reviews
+    # binding.pry
+    Review.all.select {|flick| 
+    if flick.viewer == self
+    flick
+    end}
+  end
+
+# # returns an array of Movie instances 
+# # reviewed by the Viewer instance.
+  def reviewed_movies
+    self.reviews.map {|flick| flick.movie}
+  end
 end
+
+
+
