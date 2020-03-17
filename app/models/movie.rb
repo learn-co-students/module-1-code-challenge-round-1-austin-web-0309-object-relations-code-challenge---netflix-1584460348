60208b5a35c6d5f3f13fddc0113d179139adb846
agomezdemen <agomezdemen15@gmail.com>
2020-03-17 12:57:00 -0500

    submission for deadline

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,44 @@
 class Movie
   attr_accessor :title
+  attr_reader :reviews, :reviewers
 
   @@all = []
 
   def initialize(title)
     @title = title
     self.class.all << self
+    @reviews = []
+    @reviewers = []
   end
 
   def self.all
     @@all
+  end
+
+  def reviews(review)
+    @reviews<<review
+  end
+
+  def reviewers(user)
+    @reviewers<<user
+  end
+
+  def average_rating
+    average = 0
+    @reviews.each do |n|
+      average += n.rating
+    end
+    return average / @reviews.length
+  end
+
+  def self.highest_rated
+    best = @@all[0]
+    @@all.each do |n|
+      if n.average_rating > best.average_rating
+        best = n
+      end
+    end
+    return best
   end
 
 end
