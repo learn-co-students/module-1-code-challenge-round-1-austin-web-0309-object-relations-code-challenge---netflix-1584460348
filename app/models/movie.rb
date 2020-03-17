2f0e7630bf4f2049c68f7e403e27a6f976419faa
rbaes23 <rochelleannebaes@gmail.com>
2020-03-17 12:59:49 -0500

    submission for deadline

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Movie
   attr_accessor :title
 
@@ -12,4 +14,30 @@ class Movie
     @@all
   end
 
+  def self.highest_rated
+    #returns the Movie instance with the highest average rating
+    @all.sort_by {|movies| }
+  end
+
+  def reviews
+    # returns an array of all the Review instances for the Movie
+    Review.all.map {|review| review.movie == self}
+    #returns all the reviews associated with this movie
+    #[bunch of review instances...including the viewer, movie, rating]
+  end
+
+  def reviewers
+    #returns an array of all of the Viewer instances that reviewed the Movie
+    #we grabbed all the reviews that belongs to this movie using reviews method
+    reviews.map {|review| review.viewer}
+  end
+
+  def averate_rating
+    #returns the average of all ratings for the Movie instance
+    #to average ratings, add all ratings together and divide by the total number of ratings.
+    #use reduce?
+    ratings = reviews.map {|review| review.rating}
+    sum = ratings.reduce(:+)
+    sum / ratings.count
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,27 @@
 class Review
+    attr_accessor :viewer, :movie, :rating
+    @@all = []
 
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        self.class.all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def viewer
+        #returns the Viewer instance associated with the Review instance
+        #for one instance of review, return its viewer
+        # Viewer.all.select {|viewer| viewer.reviews == self}
+        # review.viewer
+    end
+
+    def movie
+        #returns the Movie instance associated with the Review instance
+        # Movie.all.select {movie| movie.reviews == self}
+    end
 end
